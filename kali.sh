1e24f7c20d916446ba7395e02f335846a78092b4
g3g2g1 <witzzhao@gmail.com>
2018-10-20 12:10:42 +1100

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,1 +1,1 @@
-cd /storage/emulated/0/kali && su && sh /storage/emulated/0/kali/bootscript.sh /storage/emulated/0/kali/kalilinux.ext2.img
+sh /storage/emulated/0/kali/bootscript.sh /storage/emulated/0/kali/kalilinux.ext2.img
